234e9b501e44630601021fd69017711c5ef5adcb
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2020-11-13 07:33:23 -0300

    Cria classe de atendimento

---
+++ GSAtend/CamadaNegocio/Atendimento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GSAtend.CamadaNegocio
+{
+    public class Atendimento
+    {
+        public int Id { get; set; }
+        public DateTime DataAtendimento { get; set; }
+        public Paciente Paciente { get; set; }
+        public string DescricaoAtendimento { get; set; }
+    }
+}
