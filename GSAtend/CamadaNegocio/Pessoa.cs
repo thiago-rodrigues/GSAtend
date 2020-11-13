50a04ef8f6e062870c5d37ec5314824398f694d0
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2020-11-13 07:29:49 -0300

    Cria classe de pessoa

---
+++ GSAtend/CamadaNegocio/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GSAtend.CamadaNegocio
+{
+    public class Pessoa
+    {
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        
+    }
+}
