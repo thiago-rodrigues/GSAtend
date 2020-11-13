ca05378ec64ab70ce87f7fb41a91a6e9101e0e92
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2020-11-13 07:34:07 -0300

    Cria classe de Paciente

---
+++ GSAtend/CamadaNegocio/Paciente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GSAtend.CamadaNegocio
+{
+    public class Paciente : Pessoa
+    {
+        public List<Atendimento> Atendimentos;
+    }
+}
