73c677d4cfda4792f27e4c28b3c98dba9ed8dd7e
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2020-11-14 18:02:43 -0300

    adiciona classe de atendimentos e interface

---
+++ GSAtend/CamadaDB/AtendimentoDB.cs
@@ -0,0 +1,41 @@
+﻿using GSAtend.CamadaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GSAtend.CamadaDB
+{
+    public class AtendimentoDB : IAtendimento
+    {
+        public void Add(Atendimento atendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Atendimento atendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Atendimento> GetAllPacientes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Atendimento> GetAtendimentosByCPF(string CPF)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PreencheGrid(DataGridView dataGrid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Atendimento atendimento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GSAtend/CamadaDB/IAtendimento.cs
@@ -0,0 +1,18 @@
+﻿using GSAtend.CamadaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GSAtend.CamadaDB
+{
+    interface IAtendimento
+    {
+        public List<Atendimento> GetAllPacientes();
+        public List<Atendimento> GetAtendimentosByCPF(string CPF);
+        public void Add(Atendimento atendimento);
+        public void Update(Atendimento atendimento);
+        public void Delete(Atendimento atendimento);
+        public void PreencheGrid(DataGridView dataGrid);
+    }
+}
