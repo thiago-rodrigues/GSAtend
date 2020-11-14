deb7430c18702281ed405e5af86d805967bcd094
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2020-11-14 15:12:26 -0300

    adiciona formulario principal com os menus de cadastro de paciente e atendimentos

--- GSAtend/frmPrincipal.cs
+++ GSAtend/frmPrincipal.cs
@@ -7,15 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GSAtend.CamadaDB;
+using GSAtend.CamadaNegocio;
+using Sirb.Documents.BR.Validation;
 
 namespace GSAtend
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
+    public partial class frmPrincipal : Form
+    {        
+        public frmPrincipal()
         {
             InitializeComponent();
         }
 
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
     }
 }
