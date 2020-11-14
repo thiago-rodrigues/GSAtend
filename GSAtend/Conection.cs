41abaf8e0d68cd0181e3721e5530377dee016db5
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2020-11-14 06:48:34 -0300

    adiciona classe de conexão com BD

---
+++ GSAtend/Conection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace GSAtend
+{
+    public static class Conection
+    {
+        private static SqlConnection con;              
+
+        public static SqlConnection getConexao()
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["GSAtend"].ToString());
+            try
+            {  
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                    return con;
+                }
+                else
+                {
+                    return con;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erro ao tentar recuperar conexao!!\n" + e, "Erro de Conexao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return con;
+        }
+    }
+}
