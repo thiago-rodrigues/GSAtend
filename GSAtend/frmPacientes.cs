3eaa26de0900e51abb32e16d864f9ff4f5bf1aa2
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2020-11-14 21:15:20 -0300

    adiciona funcao salvar no formulario de pacientes

--- GSAtend/frmPacientes.cs
+++ GSAtend/frmPacientes.cs
@@ -1,18 +1,47 @@
-﻿using System;
+﻿using GSAtend.CamadaDB;
+using GSAtend.CamadaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Sirb.Documents.BR.Validation;
 
 namespace GSAtend
 {
     public partial class frmPacientes : Form
     {
+        private Paciente paciente = new Paciente();
+        private PacienteDB pacienteDB = new PacienteDB();
         public frmPacientes()
         {
             InitializeComponent();
         }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if ((txtcpf.Text != "   .   .   -") && (txtnome.Text != "") && (cbSexo.Text != ""))
+            {
+                paciente.CPF = Cpf.RemoveMask(txtcpf.Text);
+                paciente.Nome = txtnome.Text;
+                paciente.DataNascimento = Convert.ToDateTime(dtpdata.Text);
+                paciente.Sexo = cbSexo.Text;
+                pacienteDB.Add(paciente);
+                pacienteDB.PreencheGrid(dgMedicos);
+                btnNovo_Click(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Favor preencher todos os campos!!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
     }
 }
